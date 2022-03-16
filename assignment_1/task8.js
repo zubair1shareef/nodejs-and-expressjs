8fec1709d2aa7ac6c2f72a1bd55dba1bc28dad04
zubair1shareef <66367807+zubair1shareef@users.noreply.github.com>
2022-03-16 14:43:39 +0530

    cleanup code

--- assignment_1/task8.js
+++ assignment_1/task8.js
@@ -1,37 +1,5 @@
 const http=require('http');
-const fs=require('fs');
-
-http.createServer((req,res)=>{
-    url=req.url;
-    res.setHeader('Content-Type', 'text/html');
-    if(url=='/'){
-        res.write('<html>')
-        res.write('<head><title> home</title></head>')
-        res.write('<body>')
-        res.write('<form action="/message" method="POST" type="text"><input type="text" name="message" ></input><button type="submit">submit</button></form>')
-        res.write('</body>')
-        res.write('</head>')
-        res.end();
+const handler=require('./task8Route')
 
 
-    }
-    if(url=='/message' && req.method=='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk)
-        })
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString();
-            const message=parseBody.split('=')[1];
-            fs.writeFileSync('message.txt',message)
-            res.statusCode=302
-            res.setHeader('Location','/');
-          
-            return res.end();
-
-        })
-
-    }
-
-}).listen(3000)
+http.createServer(handler).listen(3000)
---
+++ assignment_1/task8Route.js
@@ -0,0 +1,37 @@
+const fs=require('fs');
+
+const routeHandler=(req,res)=>{
+    url=req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if(url=='/'){
+        res.write('<html>')
+        res.write('<head><title> home</title></head>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST" type="text"><input type="text" name="message" ></input><button type="submit">submit</button></form>')
+        res.write('</body>')
+        res.write('</head>')
+        res.end();
+
+
+    }
+    if(url=='/message' && req.method=='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFileSync('message.txt',message)
+            res.statusCode=302
+            res.setHeader('Location','/');
+          
+            return res.end();
+
+        })
+
+    }
+
+}
+module.exports=routeHandler
